28e663429af736dc599d2c801f473591f55e3da0
huangmeideng <huangmeideng@outlook.com>
2018-12-20 09:38:02 +0800

    下拉刷新

--- src/pages/program/index.vue
+++ src/pages/program/index.vue
@@ -14,7 +14,10 @@ export default {
     },
     data () {
         return {
-            programData: []
+            programData: [],
+            page: 1,
+            pageSize: 10,
+            pageMax: 1
         }
     },
     methods: {
@@ -31,6 +34,7 @@ export default {
                 success (res) {
                     let result = res.data
                     if(result.status === 200){
+                        that.pageMax = result.pagemax
                         for(let item of utils.oneArrayToTwoArray(result.data,2)){
                             that.programData.push(item)
                         }
@@ -43,11 +47,89 @@ export default {
                     }
                 }
             })
+        },
+        /**
+         * 获取更多节目数据
+         */
+        getMoreProgramData() {
+            this.page ++
+            if(this.page <= this.pageMax) {
+                let programpUrl = globalData.state.programUrl + "?is_past=1&page=" + this.page
+                let that = this
+                wx.request({
+                    url: programpUrl,
+                    header: {
+                        'content-type': 'application/json'
+                    },
+                    success (res) {
+                        let result = res.data
+                        if(result.status === 200) {
+                            for(let item of utils.oneArrayToTwoArray(result.data,2)) {
+                                that.programData.push(item)
+                            }
+                        } else{
+                            wx.showToast({
+                                title: '服务器出错',
+                                icon: 'warn',
+                                duration: 5000
+                            })
+                        }
+                    }
+                })
+            } else{
+                wx.showToast({
+                    title: '我是有底线的',
+                    icon: 'warn',
+                    duration: 2000
+                })
+            }
+        },
+        /**
+         * 刷新获取数据
+         */
+        getNewProgramData() {
+            this.programData = []
+            this.pageMax = 1
+            this.page = 1
+            let that = this
+            wx.request({
+                url: globalData.state.programUrl + '?is_past=1',
+                header: {
+                    'content-type': 'application/json'
+                },
+                success (res) {
+                    let result = res.data
+                    if(result.status === 200) {
+                        that.pageMax = result.pagemax
+                        for( let item of utils.oneArrayToTwoArray(result.data,2)) {
+                            that.programData.push(item)
+                        }
+                    } else{
+                        wx.showToast({
+                            title: '服务器出错',
+                            icon: 'warn',
+                            duration: 2000
+                        })
+                    }
+                }
+            })
         }
     },
     mounted() {
         this.getProgramData()
     },
+    /**
+     * 上拉加载
+     */
+    onReachBottom() {
+        this.getMoreProgramData()
+    },
+    /**
+     * 下拉刷新
+     */
+    onPullDownRefresh() {
+        this.getNewProgramData()
+    }
 }
 </script>
 
